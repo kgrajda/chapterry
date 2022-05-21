f9faa55433b91f9c2987082a763cf826c8fc2bf1
Krzysztof Grajda <krzysztof.grajda@proton.me>
2022-05-22 01:32:18 +0200

    Add eyeD3 dependency

--- mp3_chapter_manager/main.py
+++ mp3_chapter_manager/main.py
@@ -2,10 +2,18 @@ import sys
 import click
 
 
-@click.command()
-def main():
+@click.group()
+def cli():
     pass
 
 
+@click.command(help="Input MP3 file")
+@click.argument("input", type=click.Path(exists=True))
+def ls(input: click.Path):
+    pass
+
+
+cli.add_command(ls)
+
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(cli())
