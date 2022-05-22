91ba973dab42668db3dd9612a7461715f5916707
Krzysztof Grajda <krzysztof.grajda@proton.me>
2022-05-22 02:20:09 +0200

    Add chapter listing

--- mp3_chapter_manager/main.py
+++ mp3_chapter_manager/main.py
@@ -1,5 +1,8 @@
 import sys
+from typing import List
 import click
+from eyed3.id3 import Tag
+from eyed3.id3.frames import ChapterFrame, StartEndTuple
 
 
 @click.group()
@@ -10,7 +13,25 @@ def cli():
 @click.command(help="Input MP3 file")
 @click.argument("input", type=click.Path(exists=True))
 def ls(input: click.Path):
-    pass
+    tag = Tag()
+    tag.parse(input)
+
+    if tag is None:
+        click.echo("Unsupported file type.")
+        return
+
+    if tag.artist is not None:
+        click.echo(f"Artist: {tag.artist}")
+    if tag.album is not None:
+        click.echo(f"Album: {tag.album}")
+
+    chapters: List[ChapterFrame] = tag.chapters
+    for chapter in chapters:
+        times: StartEndTuple = chapter.times
+        title_part = ' "' + chapter.title + '"' if chapter.title is not None else ""
+        click.echo(
+            f"<{times.start}:{times.end}> {chapter.element_id.decode('utf-8')}{title_part}"
+        )
 
 
 cli.add_command(ls)
